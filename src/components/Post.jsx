10fabaa1af619862353b3ad8c665a4f8be4dd6ed
luizctb <luizcarlostb@hotmail.com>
2023-01-02 21:44:57 -0300

    key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,10 +9,11 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post muito bacana, hein?!'
     ])
 
+    const [newCommentText, setNewCommentText] = useState('')
+ 
     const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
     })
@@ -24,8 +25,13 @@ export function Post({ author, publishedAt, content }) {
 
     function handleCleateNewComment() {
         event.preventDefault()
+            
+        setComments([...comments, newCommentText]);   
+        setNewCommentText('');   
+    }
 
-        setComments([...comments, comments.length +1]);
+    function handleNewCommentChange () {
+        setNewCommentText (event.target.value);
     }
 
     return (
@@ -47,9 +53,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                {content.map(line => {
                     if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     }else if (line.type == 'link') {
-                        return <p><a href="#">{line.content}</a></p>;
+                        return <p key={line.content}><a href="#">{line.content}</a></p>;
                     }
                })}
             </div>
@@ -58,7 +64,10 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
                 
                 <textarea 
+                    name='comment'
                     placeholder="Deixe seu comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -68,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article>
