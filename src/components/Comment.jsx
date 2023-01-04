09aeb870e27eef143e8331ec07f018aebd98e6bf
luizctb <luizcarlostb@hotmail.com>
2023-01-03 22:06:03 -0300

    imutabilidade

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    function handuleDeleteComment () {
+        console.log ('deletar')
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/111542505?v=4" alt="" />
@@ -15,7 +20,7 @@ export function Comment({ content }) {
                             <time title="11 de Maio as 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handuleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
