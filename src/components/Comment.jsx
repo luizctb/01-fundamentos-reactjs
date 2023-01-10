9640ffd534e22d980d46ff4f7a7644383143f026
luizctb <luizcarlostb@hotmail.com>
2023-01-10 20:11:34 -0300

    closures

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+    
     function handuleDeleteComment () {
         console.log ('deletar')
         onDeleteComment(content);
+    }
+
+    function handuleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -29,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handuleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
 
